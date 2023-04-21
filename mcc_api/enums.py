8df91d62c157cc5f5a9498a59efa597e4538e4c9
James C <3661681+JamesMCo@users.noreply.github.com>
2023-04-21 01:05:14 +0100

    Adjust Game and Team enums to use custom UpperStrEnum class
    enum.StrEnum was added in Python 3.11, but since this module supports 3.10 this cannot be used. Instead, we can implement our own enum class to take its place. This also means that we can change the implementation of "_generate_next_value_" to allow us to return upper case values, and therefore we can use "auto()" instead of duplicating the member names as strings.

--- mcc_api/enums.py
+++ mcc_api/enums.py
@@ -1,44 +1,60 @@
-from enum import StrEnum
+from enum import auto, Enum
+import typing as t
 
 
-class Game(StrEnum):
+class UpperStrEnum(Enum):
+    """Custom Enum class that acts like :class:`enum.StrEnum`, using uppercase strings."""
+    def __str__(self):
+        """Return the member value when asked for the string version of the member.
+
+        As these enums use auto(), they use _generate_next_value_ and are thus guaranteed to be uppercase.
+        """
+        return self.value
+
+    @staticmethod
+    def _generate_next_value_(name: str, start: t.Optional[int], count: int, last_values: list[str]):
+        """As with :class:`enum.StrEnum`, return the member name. However, return it as its uppercase version."""
+        return name.upper()
+
+
+class Game(UpperStrEnum):
     """Game identifiers accepted and returned by the MCC API."""
 
-    MG_ROCKET_SPLEEF = "MG_ROCKET_SPLEEF"
-    MG_SURVIVAL_GAMES = "MG_SURVIVAL_GAMES"
-    MG_PARKOUR_WARRIOR = "MG_PARKOUR_WARRIOR"
-    MG_ACE_RACE = "MG_ACE_RACE"
-    MG_BINGO_BUT_FAST = "MG_BINGO_BUT_FAST"
-    MG_TGTTOSAWAF = "MG_TGTTOSAWAF"
-    MG_SKYBLOCKLE = "MG_SKYBLOCKLE"
-    MG_SKY_BATTLE = "MG_SKY_BATTLE"
-    MG_HOLE_IN_THE_WALL = "MG_HOLE_IN_THE_WALL"
-    MG_BATTLE_BOX = "MG_BATTLE_BOX"
-    MG_BUILD_MART = "MG_BUILD_MART"
-    MG_SANDS_OF_TIME = "MG_SANDS_OF_TIME"
-    MG_DODGEBOLT = "MG_DODGEBOLT"
-    MG_PARKOUR_TAG = "MG_PARKOUR_TAG"
-    MG_GRID_RUNNERS = "MG_GRID_RUNNERS"
-    MG_MELTDOWN = "MG_MELTDOWN"
-    GLOBAL_STATISTICS = "GLOBAL_STATISTICS"
-    LEGACY_STATISTICS = "LEGACY_STATISTICS"
-    MG_LOCKOUT_BINGO = "MG_LOCKOUT_BINGO"
-    MG_FOOT_RACE = "MG_FOOT_RACE"
-    MG_ROCKET_SPLEEF_OLD = "MG_ROCKET_SPLEEF_OLD"
+    MG_ROCKET_SPLEEF = auto()
+    MG_SURVIVAL_GAMES = auto()
+    MG_PARKOUR_WARRIOR = auto()
+    MG_ACE_RACE = auto()
+    MG_BINGO_BUT_FAST = auto()
+    MG_TGTTOSAWAF = auto()
+    MG_SKYBLOCKLE = auto()
+    MG_SKY_BATTLE = auto()
+    MG_HOLE_IN_THE_WALL = auto()
+    MG_BATTLE_BOX = auto()
+    MG_BUILD_MART = auto()
+    MG_SANDS_OF_TIME = auto()
+    MG_DODGEBOLT = auto()
+    MG_PARKOUR_TAG = auto()
+    MG_GRID_RUNNERS = auto()
+    MG_MELTDOWN = auto()
+    GLOBAL_STATISTICS = auto()
+    LEGACY_STATISTICS = auto()
+    MG_LOCKOUT_BINGO = auto()
+    MG_FOOT_RACE = auto()
+    MG_ROCKET_SPLEEF_OLD = auto()
 
 
-class Team(StrEnum):
+class Team(UpperStrEnum):
     """Team identifiers accepted and returned by the MCC API."""
 
-    RED = "RED"
-    ORANGE = "ORANGE"
-    YELLOW = "YELLOW"
-    LIME = "LIME"
-    GREEN = "GREEN"
-    CYAN = "CYAN"
-    AQUA = "AQUA"
-    BLUE = "BLUE"
-    PURPLE = "PURPLE"
-    PINK = "PINK"
-    SPECTATORS = "SPECTATORS"
-    NONE = "NONE"
+    RED = auto()
+    ORANGE = auto()
+    YELLOW = auto()
+    LIME = auto()
+    GREEN = auto()
+    CYAN = auto()
+    AQUA = auto()
+    BLUE = auto()
+    PURPLE = auto()
+    PINK = auto()
+    SPECTATORS = auto()
+    NONE = auto()
